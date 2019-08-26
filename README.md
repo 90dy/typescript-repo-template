5173580d953fb270daff34cfff4040f6cb1e1911
Godefroy Ponsinet <godefroy.ponsinet@outlook.com>
2019-08-27 00:02:48 +0200

    chore: first commit
    Signed-off-by: Godefroy Ponsinet <godefroy.ponsinet@outlook.com>

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # typescript-repo-template
-Template for typescript repository
+
+Template to dev everythings in typescript
+
+# Installation
+
+### With npm
+
+```bash
+$> npm install -g typescript-repo-template
+```
+
+### With yarn
+```bash
+$> yarn global add typescript-repo-template
+```
+
+# Usage
+
+```bash
+typescript-repo-template version 0.0.0-alpha.0
+
+Usage:
+  typescript-repo-template --help
+  typescript-repo-template --version
+
+Template to dev everythings in typescript
+
+Arguments:
+  protos                      Protos input files
+
+Options:
+  -h --help                   Show this
+  -v --version                Show current version
+```
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import * as child_process from 'child_process';
+import { docopt } from 'docopt';
+
+import * as helpers from '../helper';
+import {
+  description as pkgDescription,
+  version as pkgVersion,
+} from '../package.json';
+
+const doc = `typescript-repo-template version ${pkgVersion}
+
+Usage:
+  typescript-repo-template --help
+  typescript-repo-template --version
+
+${pkgDescription}
+
+Options:
+  -h --help                   Show this
+  -v --version                Show current version`;
+
+declare class TypescriptRepoTemplateOptions {
+  '--help': boolean;
+  '--version': boolean;
+}
+
+function help(): number {
+  console.log(doc);
+  return 0;
+}
+
+function version(): number {
+  console.log(`typescript-repo-template version ${pkgVersion}`);
+  return 0;
+}
+
+export async function main(argv: string[]): Promise<number> {
+  const options: TypescriptRepoTemplateOptions = docopt(doc, {
+    argv: process.argv.slice(2),
+    help: false,
+  });
+
+  if (options['--help'] !== false) {
+    return help();
+  }
+
+  if (options['--version'] !== false) {
+    return version();
+  }
+
+  return 0;
+}
